712d102aebd866561ca6e72deccde51169bda620
Ela3asr <mohammed_31079443@ics.tanta.edu.eg>
2022-12-14 10:04:42 +0200

    init

---
+++ programe.cs
@@ -0,0 +1,82 @@
+using System;
+namespace Programe;
+public class Programe
+{
+    private static void Main()
+    {
+        // Task 1   ( Reverse a String )
+        // Console.WriteLine("please Enter a string : ");
+        // var x=Console.ReadLine();
+        // Console.Write("The reverse : ");
+        // for(int i=x.Length-1 ; i>=0 ;i--){
+        //    Console.Write(x[i]);
+
+        // }
+
+        //----------------------------------------------------------------------------------------------------
+
+        //Task 2  (Count of a ’ s)
+        // Console.WriteLine("please Enter a string : ");
+        // var x=Console.ReadLine();
+        // var count=0;
+        // for(int i=0; i<x.Length;i++){
+        //      if (x[i]=='a'){
+        //           count++ ;
+        //      }
+        // }    
+        // Console.Write("The count of a : "+count);
+        //-----------------------------------------------------------------------------------------------------
+
+        //Task 3 (Last index of a)
+        //    Console.WriteLine("please Enter a string : ");
+        //    var x = Console.ReadLine();
+        //    Console.Write(x.LastIndexOf('a'));
+        //--------------------------------------------------------------------------------------------------------
+
+        // Task 4 ( Remove all a )  
+        // Console.WriteLine("Entar a string : ");
+        // String x = Console.ReadLine();
+        // String[] arr = x.Split('a');
+        // Console.Write("A string without a : ");
+        // foreach (String s in arr)
+        //     Console.Write(s);
+        //---------------------------------------------------------------------------------------------------------\
+
+        //Task 5 ( Add 0 after a)
+        // Console.WriteLine("Entar a string : ");
+        // String x = Console.ReadLine();
+        // for (int i = 0; i < x.Length; i++)
+        // {
+        //     if (x[i] == 'a')
+        //     {
+        //         x=x.Insert(i + 1, "0");
+
+        //     }
+        // }
+        // Console.Write(x);
+        //-------------------------------------------------------------------------------------------------------------
+
+        // Task 6 (count of non-literals) 
+        // Console.WriteLine("Entar a text : ");
+        // var x = Console.ReadLine();
+        // var count = 0;
+        // for (int i = 0; i < x.Length; i++)
+        // {
+        //     if (char.IsNumber(x[i]))
+        //     {
+        //         count++;
+
+        //     }
+
+        // }
+        // Console.Write(count);
+        //--------------------------------------------------------------------------------------------------------------
+
+        
+
+
+
+    }
+
+}
+
